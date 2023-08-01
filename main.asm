66360504d4329ccefdb27e4ecdb963b8010d4a7a
ebreyaue <hello@emmanuelbreyaue.com>
2023-08-01 15:38:41 -0300

    only monochrome at the firsts versions.

--- main.asm
+++ main.asm
@@ -4,16 +4,8 @@
 bits 16
 org 0x7c00
 
-;set background
-;mov ah, 0x06
-;xor al, al
-;xor cx, cx
-;mov dx, 0x184f
-;mov dh, 0x4e
-;int 0x10
-
 ;move label 
-mov si, bootloaderbanner ;bootloaderbanner
+mov si, blbanner ;bootloaderbanner
 mov ah, 0x0e
 loop:
 lodsb
@@ -24,7 +16,7 @@ jmp loop
 
 end:
 hlt
-bootloaderbanner: db "             ____       ,6P",13,10,"            6MMMMb    6MM'",13,10,"           6M'  `Mb  6M'",13,10,"____   ___ MM    M9 6M ____",13,10,"`MM(   )P' YM.  ,9  MMMMMMMb",13,10," `MM` ,P    YMMMMb  MM'   `Mb",13,10,"  `MM,P     6'  `Mb MM     MM",13,10,"   `MM.    6M    MM MM     MM",13,10,"   d`MM.   MM    MM MM     MM",13,10,"  d' `MM.  YM.  ,M9 YM.   ,M9",13,10,"_d_  _)MM_  YMMMM9   YMMMMM9",13,10,"   ",13,10,"   Bootloader Assembly Language",13,10,"   Develop by @nerdemma at emmanuelbreyaue.com",0 
+blbanner: db "             ____       ,6P",13,10,"            6MMMMb    6MM'",13,10,"           6M'  `Mb  6M'",13,10,"____   ___ MM    M9 6M ____",13,10,"`MM(   )P' YM.  ,9  MMMMMMMb",13,10," `MM` ,P    YMMMMb  MM'   `Mb",13,10,"  `MM,P     6'  `Mb MM     MM",13,10,"   `MM.    6M    MM MM     MM",13,10,"   d`MM.   MM    MM MM     MM",13,10,"  d' `MM.  YM.  ,M9 YM.   ,M9",13,10,"_d_  _)MM_  YMMMM9   YMMMMM9",13,10,"   ",13,10,"   Bootloader Assembly Language",13,10,"   Develop by @nerdemma at emmanuelbreyaue.com",0 
 
 times 510-($-$$) db 0
 dw 0xAA55
