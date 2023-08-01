5707c83f4ecd27e58808af89acd543cd42bb1a4f
ebreyaue <hello@emmanuelbreyaue.com>
2023-08-01 15:27:09 -0300

    reprogramming to the basic functions.

--- main.asm
+++ main.asm
@@ -1,105 +1,30 @@
-BITS 16 
-mov ax, 07C0h
-add ax, 288
-mov ss, ax
-mov sp, 4096
-mov ax, 07C0h
-mov ds, ax
+;bootloader x86 develop by emmanuel.d.breyaue
+;hello[at]emmanuelbreyaue.com
 
-mov si, msg
-call print_string
+bits 16
+org 0x7c00
 
-mov si, msg
-call print_string
-call read_command
-jmp $
+;set background
+;mov ah, 0x06
+;xor al, al
+;xor cx, cx
+;mov dx, 0x184f
+;mov dh, 0x4e
+;int 0x10
 
-msg db 'Bienvenido a Bootloader', 13, 10, 0
-msgb db 'Buen Dia Emma', 13 ,10 , 0
-
-command db 20 dup(0)
-
-print_string:
-mov ah, 0x0E
-mov bh, 0x00
-
-.repeat:
+;move label 
+mov si, bootloaderbanner ;bootloaderbanner
+mov ah, 0x0e
+loop:
 lodsb
-cmp al, 0
-je .done
+test al, al
+jz end
 int 0x10
-jmp .repeat
-.done:
-ret 
+jmp loop
 
-read_command:
-mov ah, 0x0A
-mov dx, command
-int 0x21
-cmp byte ptr [command], 'h'
-je .print_hour
-cmp byte ptr [command], 'm'
-je .print_memory
-jmp invalid_command
+end:
+hlt
+bootloaderbanner: db "             ____       ,6P",13,10,"            6MMMMb    6MM'",13,10,"           6M'  `Mb  6M'",13,10,"____   ___ MM    M9 6M ____",13,10,"`MM(   )P' YM.  ,9  MMMMMMMb",13,10," `MM` ,P    YMMMMb  MM'   `Mb",13,10,"  `MM,P     6'  `Mb MM     MM",13,10,"   `MM.    6M    MM MM     MM",13,10,"   d`MM.   MM    MM MM     MM",13,10,"  d' `MM.  YM.  ,M9 YM.   ,M9",13,10,"_d_  _)MM_  YMMMM9   YMMMMM9",13,10,"   ",13,10,"   Bootloader Assembly Language",13,10,"   Develop by @nerdemma at emmanuelbreyaue.com",0 
 
-.print_hour:
-mov ah, 0x02
-int 0x1A
-mov bh, 0x00
-mov dl, ch
-call print_hex
-mov dl, cl
-call print_hex
-jmp command_done
-
-.print_memory:
-mov ah, 0x48
-xor bx, bx
-int 0x21
-mov ah, 0x02
-mov dl, bl
-call print_hex
-jmp command_done
-
-print_hex:
-push ax
-push bx
-push cx
-push dx
-mov bl, dl
-shr dl, 4
-add dl, '0'
-add dl, '9'
-jle .skip_adjust
-add dl, 7
-
-.skip_adjust:
-int 0x10
-mov dl, bl 
-and dl, 0x0F
-add dl, '0'
-cmp dl, '9'
-jle .skip_adjust2
-add dl, 7
-
-.skip_adjust2:
-int 0x10
-pop dx
-pop cx
-pop bx
-pop ax
-ret
-
-command_done:
-mov si, command
-call print_string
-ret
-
-invalid_command:
-mov si, invalid
-call print_string
-ret
-
-invalid db 'Comando Invalido',13,10,0
 times 510-($-$$) db 0
 dw 0xAA55
